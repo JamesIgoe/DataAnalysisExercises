4f21afd21ef2ac7857ffae14844ebb7fc9582c54
James Igoe <james.igoe@gmail.com>
2022-10-27 02:26:02 -0400

    removed problematic work

--- ComputationalStatisticsInPython/Exercises_3_FunctionalProgramming.py
+++
@@ -1,9 +0,0 @@
-"""http://people.duke.edu/~ccc14/sta-663/FunctionsSolutions.html#the-toolz-fn-and-funcy-modules"""
-"""If you wish to program in the functional style, check out the following packages, toolz, fn, funcy"""
-
-
-
-
-
-
-
